339f5b18f318e7ba295d9b5f96be9d5777c975e1
Phlippie Bosman <phlippie.bosman@gmail.com>
2013-05-14 14:16:28 +0200

    Basic encryption works. Format is input: text (String), output: list of numbers

--- src/cos720a3/Encrypt.java
+++ src/cos720a3/Encrypt.java
@@ -12,7 +12,15 @@ package cos720a3;
 public class Encrypt {
 
     public static void main (String [] args) throws Exception {
-        Encryption.encryptAsString("abcdefghijklmnop", null);
+        String publicKeyFileName = args[0];
+        PublicKey k = KeyFileIO.readPublicKeyFromFilename(publicKeyFileName);
+        Integer [] encrypted = Encryption.encryptTextStringAsNumbers("abcdefghijklmnop", k);
+        
+        System.out.print("Encrypted value(s): [");
+        for (int i = 0; i < encrypted.length; i++) {
+            System.out.print(encrypted[i] + " ");
+        }
+        System.out.println("]");
 
     }
 
--- src/cos720a3/Encryption.java
+++ src/cos720a3/Encryption.java
@@ -1,16 +1,40 @@
 package cos720a3;
 
+import java.util.ArrayList;
+
 /**
  * Implementation of the Merkle-Hellman encryption.
  * @author phlippie
  */
 public class Encryption {
-    public static String encryptAsString (String plaintext, PublicKey key) {
-        String bString = Util.stringToBinaryString(plaintext);
-        System.out.println(bString);
-        String nString = Util.binaryStringToString(bString);
-        System.out.println(nString);
-        return "";
+    public static Integer [] encryptTextStringAsNumbers (String plaintext, PublicKey key) {
+        String bPlaintext = Util.stringToBinaryString(plaintext);
+        Integer [] result;
+
+        int keySize = key.knapsack.size();
+        int numberOfPasses = bPlaintext.length() / keySize;
+        result = new Integer [numberOfPasses];
+        for (int i = 0; i < numberOfPasses; i++ ) {
+            String bPlaintextSubstring = bPlaintext.substring(i*keySize,(i+1)*keySize);
+            result[i] = encryptBlock(bPlaintextSubstring, key);
+        }
+
+
+        return result;
+    }
+
+    public static Integer encryptBlock(String binaryBlock, PublicKey key) {
+        ArrayList<Integer> C = new ArrayList<Integer>();
+        for (int i = 0; i < key.knapsack.size(); i++) {
+            if (binaryBlock.charAt(i) == '1') {
+                C.add(key.knapsack.get(i));
+            }
+        }
+        Integer blockTotal = 0;
+        for (int i = 0; i < C.size(); i++) {
+            blockTotal += C.get(i);
+        }
+        return blockTotal;
     }
 
 }
